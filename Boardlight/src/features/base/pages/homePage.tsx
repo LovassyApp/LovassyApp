d6cd0d7da7a374c1e6343463090992b5cb4296a0
Xeretis <70800211+Xeretis@users.noreply.github.com>
2023-09-06 14:21:45 +0200

    Added a "construction sign" to the home page

--- Boardlight/src/features/base/pages/homePage.tsx
+++ Boardlight/src/features/base/pages/homePage.tsx
@@ -1,14 +1,32 @@
-import { Box, Text, createStyles } from "@mantine/core";
+import { Box, Center, Stack, Text, Title, createStyles, rem } from "@mantine/core";
 
+import { IconBarrierBlock } from "@tabler/icons-react";
 import { useGetApiProducts } from "../../../api/generated/features/products/products";
 
-const useStyles = createStyles((theme) => ({}));
+const useStyles = createStyles((theme) => ({
+    center: {
+        height: "100%",
+    },
+}));
 
 const HomePage = (): JSX.Element => {
     const { classes } = useStyles();
-    const produts = useGetApiProducts();
 
-    return <Text>Test</Text>;
+    return (
+        <Center className={classes.center}>
+            <Stack spacing={0}>
+                <Center>
+                    <IconBarrierBlock size={rem(124)} />
+                </Center>
+                <Title align="center" px="sm">
+                    Kezdőlap építés alatt
+                </Title>
+                <Text color="dimmed" align="center" px="sm">
+                    Ez az oldal jelenleg még építés alatt áll... Amíg vársz nézd meg addig mi mindent tud a többi oldal!
+                </Text>
+            </Stack>
+        </Center>
+    );
 };
 
 export default HomePage;
