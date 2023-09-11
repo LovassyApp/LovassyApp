abc1aa2617c6ed6220a5b666123f8bd50836fdfe
Máté Gyimesi <gyim.mate@gmail.com>
2023-09-11 13:57:54 +0200

    Remove .env dependency on production

--- Boardlight/src/api/customClient.ts
+++ Boardlight/src/api/customClient.ts
@@ -1,6 +1,6 @@
-import { getPostApiAuthRefreshMutationOptions } from "./generated/features/auth/auth";
-import { handleApiErrors } from "../helpers/apiHelpers";
-import { useAuthStore } from "../core/stores/authStore";
+import { getPostApiAuthRefreshMutationOptions } from './generated/features/auth/auth';
+import { handleApiErrors } from '../helpers/apiHelpers';
+import { useAuthStore } from '../core/stores/authStore';
 
 const callAPI = async (url: string, init: RequestInit) => {
     const response = await fetch(url, init);
@@ -16,15 +16,15 @@ const callAPI = async (url: string, init: RequestInit) => {
     }
 
     try {
-        if (response.headers.get("Content-Type")?.includes("application/json")) {
+        if (response.headers.get('Content-Type')?.includes('application/json')) {
             return await response.json();
         }
-        const a = document.createElement("a");
+        const a = document.createElement('a');
         const file = window.URL.createObjectURL(await response.blob());
         a.href = file;
-        const header = response.headers.get("Content-Disposition");
-        const parts = header.split(";");
-        const filename = parts[1].split("=")[1];
+        const header = response.headers.get('Content-Disposition');
+        const parts = header.split(';');
+        const filename = parts[1].split('=')[1];
         a.download = filename;
         a.click();
     } catch (error) {
@@ -43,19 +43,19 @@ export const useCustomClient = async <T>({
     signal,
 }: {
     url: string;
-    method: "get" | "post" | "put" | "delete" | "patch";
+    method: 'get' | 'post' | 'put' | 'delete' | 'patch';
     params?: any;
     headers?: any;
     data?: unknown;
     signal?: AbortSignal;
 }): Promise<T> => {
-    const baseUrl = import.meta.env.VITE_BLUEBOARD_URL;
+    const baseUrl = import.meta.env.VITE_BLUEBOARD_URL ?? '';
 
     let headersLocal;
     if (data instanceof FormData) {
-        headersLocal = { Accept: "application/json" };
+        headersLocal = { Accept: 'application/json' };
     } else {
-        headersLocal = { ...headers, "Content-Type": "application/json", Accept: "application/json" };
+        headersLocal = { ...headers, 'Content-Type': 'application/json', Accept: 'application/json' };
     }
 
     if (useAuthStore.getState().accessToken) {
@@ -71,7 +71,7 @@ export const useCustomClient = async <T>({
     } catch (error) {
         if (error.status === 401) {
             try {
-                console.log("Attempting to refresh token...");
+                console.log('Attempting to refresh token...');
 
                 // eslint-disable-next-line react-hooks/rules-of-hooks
                 const refreshOptions = getPostApiAuthRefreshMutationOptions();
@@ -80,7 +80,7 @@ export const useCustomClient = async <T>({
                 useAuthStore.getState().setAccessToken(res.token);
                 headersLocal.Authorization = `Bearer ${res.token}`;
 
-                console.log("Successfully refreshed token");
+                console.log('Successfully refreshed token');
 
                 return await callAPI(`${baseUrl}${url}?${new URLSearchParams(params)}`, {
                     method: method.toUpperCase(),
@@ -88,10 +88,10 @@ export const useCustomClient = async <T>({
                     ...(data ? { body: data instanceof FormData ? data : JSON.stringify(data), signal } : { signal }),
                 });
             } catch (error) {
-                console.log("Failed to refresh token");
+                console.log('Failed to refresh token');
 
                 useAuthStore.getState().setAccessToken(undefined);
-                document.dispatchEvent(new Event("resetQueryClient")); // You may ask why not just call queryClient.clear() here... Well, the thing is that even though that works perfectly, I can't for the love of god get it to generate the client if we do that. So we have this weird ass solution instead, and we listen to this event in main.tsx.
+                document.dispatchEvent(new Event('resetQueryClient')); // You may ask why not just call queryClient.clear() here... Well, the thing is that even though that works perfectly, I can't for the love of god get it to generate the client if we do that. So we have this weird ass solution instead, and we listen to this event in main.tsx.
             }
         }
         handleApiErrors(error);
