80aef6e4a20d10f61a5fff5a64142bb3231cae4a
Menyhárt Máté <mateka.mateka07@gmail.com>
2025-12-30 20:09:42 +0100

    Created IncrementYear page

---
+++ Boardlight/src/features/increment/pages/incrementYearPage.tsx
@@ -0,0 +1,94 @@
+import {
+    Button,
+    Center,
+    Modal,
+    Text,
+    TextInput,
+    Title,
+    createStyles,
+    useMantineTheme,
+} from "@mantine/core";
+import { IconCheck, IconCopy, IconPlus, IconX } from "@tabler/icons-react";
+import { ValidationError, handleValidationErrors } from "../../../helpers/apiHelpers";
+import {
+    postApiIncrement,
+    usePostApiIncrement,
+} from "../../../api/generated/features/increment/increment";
+import { useEffect, useMemo, useState } from "react";
+
+import { IncrementIncrementYearRequestBody } from "../../../api/generated/models";
+import { PermissionRequirement } from "../../../core/components/requirements/permissionsRequirement";
+import { notifications } from "@mantine/notifications";
+import { useDisclosure } from "@mantine/hooks";
+import { useForm } from "@mantine/form";
+import { useGetApiAuthControl } from "../../../api/generated/features/auth/auth";
+import { useQueryClient } from "@tanstack/react-query";
+
+const useStyles = createStyles(() => ({
+    center: {
+        height: "100%",
+    },
+}));
+
+const CreateImportKeyModal = ({ opened, close }: { opened: boolean; close(): void }): JSX.Element => {
+    const incrementYear = usePostApiIncrement();
+
+    const form = useForm({
+        initialValues: {
+            confirmationText: "",
+        },
+    });
+
+    const submit = form.onSubmit(async (values) => {
+        try {
+            await incrementYear.mutateAsync({
+                data: values,
+            });
+            notifications.show({
+                title: "Tanév léptetve",
+                message: "A tanév léptetése sikeres.",
+                color: "green",
+                icon: <IconCheck />,
+            });
+            close();
+        } catch (err) {
+            if (err instanceof ValidationError) handleValidationErrors(err, form);
+        }
+    });
+
+    return (
+        <Modal opened={opened} onClose={close} title="Tanév léptetése" size="lg">
+            <form onSubmit={submit}>
+                <Text mb="md">A folytatáshoz írd be az alábbi mezőbe a következő szöveget: „<i>Mit sütsz, kis szűcs? Sós húst sütsz, kis szűcs?</i>”</Text>
+                <TextInput label="Megerősítő szöveg" required={true} {...form.getInputProps("confirmationText")} mb="sm" placeholder="Mit sütsz, kis szűcs? Sós húst sütsz, kis szűcs?"/>
+                <Button type="submit" fullWidth={true} mt="sm" color="red">
+                    Tanév léptetése
+                </Button>
+            </form>
+        </Modal>
+    );
+};
+
+const IncrementYearPage = (): JSX.Element => {
+    const { classes } = useStyles();
+    const theme = useMantineTheme();
+
+
+    const [createImportKeyModalOpened, { close: closeCreateImportKeyModal, open: openCreateImportKeyModal }] = useDisclosure(false);
+
+    return (
+        <>
+            <CreateImportKeyModal opened={createImportKeyModalOpened} close={closeCreateImportKeyModal} />
+            <Title mb="md">Tanév léptetése</Title>
+            <Text color="dimmed">
+                Az alábbi gombbal új tanévet lehet kezdeni. Ekkor az adatbázisból törlődnek az előző tanévben szerzett osztályzatok, lolók; az évfolyamok léptetésre kerülnek.
+            </Text>
+            <Text color="dimmed" mb="md">A gombot használd körültekintően!</Text>
+            <Center>
+                <Button color="red" onClick={() => openCreateImportKeyModal()}>Tanév léptetése</Button>
+            </Center>
+        </>
+    );
+};
+
+export default IncrementYearPage;
